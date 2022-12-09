0b8b5c6f306f47254c6db74bfab433f38245e889
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-12-08 23:26:25 -0500

    initial commit

---
+++ commands/ping.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply({ content: '🏓 Pinging...', fetchReply: true }).then(
+			(msg) =>
+			msg.edit({
+				content: null,
+				embeds: [
+					{
+					title: '🏓  Ping!',
+					color: 0xE38271,
+					fields: [
+						{ name: 'WebSocket Ping', value: `${interaction.client.ws.ping}ms`, inline: true },
+						{ name: '\u200B', value: '\u200B', inline: true },
+						{ name: 'API Latency', value: `${(Date.now() - msg.createdTimestamp) * -1}ms`, inline: true },
+					],
+					},
+				],
+			}),
+		);
+	},
+};
