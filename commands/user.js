f470fc0ba03284665cda9d855ab2dc847cc4edf6
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-12-09 23:52:25 -0500

    a secret little update

--- commands/user.js
+++ commands/user.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { userMention } = require('@discordjs/builders');
 
 module.exports = {
@@ -18,6 +18,16 @@ module.exports = {
 				{ name: 'User', value: userMention(interaction.member.id), inline: true },
 				{ name: '\u200B', value: '\u200B', inline: true },
 				{ name: 'Joined Discord', value: '<t:' + Math.round(joinedAt.getTime() / 1000) + ':d>', inline: true },
+			];
+		}
+		else if (interaction.options.getUser('target').id === '595412974621949972') {
+			joinedAt = new Date(interaction.options.getUser('target').createdAt);
+			dynamicEmbed.title = 'What\'s this, a dedicated page just for the love of my life? yes <3';
+			dynamicEmbed.fields = [
+				{ name: 'User', value: userMention(interaction.options.getUser('target').id), inline: true },
+				{ name: '\u200B', value: '\u200B', inline: true },
+				{ name: 'Joined Discord', value: '<t:' + Math.round(joinedAt.getTime() / 1000) + ':d>', inline: true },
+				{ name: 'A note to my love', value: 'Every moment of every passing hour you are on my mind. For I long to see you every second of the day and wish to hold you close every chance I get. The thoughts of you that fill my head are like no other I\'ve experienced. To think that someone so beautiful would have entered my life so early boggles my mind. I have thought many times over how stunning you are. By which I mean all of you, how wondrous your personality is which attracted me in the first place, to how absolutely stunningly gorgeous you are. I want to experience all the world has to offer with you, as my best friend and the love of my life.' },
 			];
 		}
 		else {
