8173daed2ff9d658ea5e2ad7a44f6f28dd69cc8d
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-12-10 09:54:44 -0500

    redefined feature categories

--- commands/help.js
+++ commands/help.js
@@ -12,31 +12,29 @@ module.exports = {
 					{ name: 'General', value: 'General' },
 					{ name: 'Personal', value: 'Personal' },
 					{ name: 'Anime', value: 'Anime' },
+                    { name: 'Utility', value: 'Utility' },
                     { name: 'Settings', value: 'Settings' },
-                    { name: 'Custom-Commands', value: 'Custom-Commands' },
 				)),
     async execute(interaction) {
         if (interaction.options.getString('category') === null) {
             dynamicEmbed.title = '📚 Help';
             dynamicEmbed.fields = [
-                    { name: ':lotus: General', value: 'Default commands that provide statistical information.', inline: true },
+                    { name: ':lotus: General', value: ' Some generic bot commands.', inline: true },
                     { name: ':person_pouting: Personal', value: 'Your discord and bot information.', inline: true },
                     { name: ':shinto_shrine: Anime', value: 'Lookup anime and discuss them with your friends!', inline: true },
                     { name: '\u200B', value: '\u200B' },
+                    { name: ':wrench: Utility', value: 'Commands that provide statistical information', inline: true },
                     { name: ':gear: Settings', value: 'Configuration tools for a custom experience.', inline: true },
-                    { name: ':wrench: Custom Commands', value: 'Further customize your experience with custom commands!', inline: true },
                     { name: '\u200B', value: '\u200B' },
                     { name: 'ℹ Slash Commands', value: '*Kumiko makes use of slash commands now!* (`/` *instead of* `!k`)\n*Reminder that commands can be disabled or enabled in your* `Server Settings → Integrations → Kumiko`' }];
         }
         else {
             switch (interaction.options.getString('category').toLowerCase()) {
                 case 'general':
-                    dynamicEmbed.title = ':gear: General';
+                    dynamicEmbed.title = ':lotus: General';
                     dynamicEmbed.description = 'General Commands';
                     dynamicEmbed.fields = [
                         { name: '/help', value: 'Lists all categories and their commands.', inline: true },
-                        { name: '/info', value: 'Displays Kumiko\'s bot description!', inline: true },
-                        { name: '/ping', value: 'Pings for the bot / API latency.', inline: true },
                         { name: '/server', value: 'Displays guild information.' }];
                     break;
                 case 'personal':
@@ -50,14 +48,16 @@ module.exports = {
                     dynamicEmbed.description = 'Anime Commands';
                     dynamicEmbed.fields = [];
                     break;
+                case 'utility':
+                    dynamicEmbed.title = ':wrench: Utility';
+                    dynamicEmbed.description = 'Custom Command Settings';
+                    dynamicEmbed.fields = [
+                        { name: '/info', value: 'Displays Kumiko\'s bot description!', inline: true },
+                        { name: '/ping', value: 'Pings for the bot / API latency.', inline: true }];
+                    break;
                 case 'settings':
                     dynamicEmbed.title = ':gear: Settings';
                     dynamicEmbed.description = 'Settings Commands';
-                    dynamicEmbed.fields = [];
-                    break;
-                case 'custom-commands':
-                    dynamicEmbed.title = ':wrench: Custom Commands';
-                    dynamicEmbed.description = 'Custom Command Settings';
                     dynamicEmbed.fields = [];
                     break;
             }
