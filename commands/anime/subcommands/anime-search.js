373aa393999a48e7a587bba4eed462d62837f6d3
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-12-16 22:26:02 -0500

    replaced synopsis length check

--- commands/anime/subcommands/anime-search.js
+++ commands/anime/subcommands/anime-search.js
@@ -5,6 +5,15 @@ module.exports = {
     async animeSearch(interaction) {
         const info = malScraper.getInfoFromName(interaction.options.getString('anime'), true);
         const data = await info;
+        let synopsis;
+
+        if (data.synopsis.length > 1021) { // mal rewrite text is 28 characters long
+            synopsis = data.synopsis.substring(0, 1024 - 29);
+            synopsis = synopsis.concat('...\n\n[Written by Mal Rewrite]');
+        }
+        else {
+            synopsis = data.synopsis;
+        }
 
         const embed = new EmbedBuilder()
             .setColor(0xF5B5C8)
@@ -13,9 +22,10 @@ module.exports = {
             .setThumbnail(data.picture)
             .setDescription(data.score + ` score (${data.scoreStats}) • ${data.type}`)
             .addFields(
+                { name: 'Aired', value: `${data.premiered}`, inline: true },
                 { name: 'status', value: `${data.status}`, inline: true },
                 { name: 'episodes', value: `${data.episodes} episodes`, inline: true },
-                { name: 'synopsis', value: data.synopsis },
+                { name: 'synopsis', value: synopsis },
             );
         await interaction.reply({ embeds: [embed] });
     },
