c6c69fa70c51a59a2a389ed40645c3bead3ad85a
AKR0SS <59298840+AKR0SS@users.noreply.github.com>
2022-12-21 14:23:47 -0500

    refactored for decluttering of root dir

--- functions/deploy-commands.js
+++ functions/deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('../config.json');
 const fs = require('node:fs');
 
 const commands = [];
@@ -7,15 +7,15 @@ const commands = [];
 const commandFolder = fs.readdirSync('./commands');
 
 console.log('\n%s %s Reading command files at %s.', '\x1b[46m STARTED \x1b[0m', '\x1b[34m[DEPLOY-COMMANDS]\x1b[0m', commandFolder);
+
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const folder of commandFolder) {
-
 	try {
 		const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith('.js'));
 
 		for (const file of commandFiles) {
-			const command = require(`./commands/${folder}/${file}`);
-			console.info(`%s %s ./commands/${folder}/${file}`, '\x1b[42m DEPLOY \x1b[0m', '\x1b[34m[FILE_PATH]\x1b[0m');
+			const command = require(`../commands/${folder}/${file}`);
+			console.info(`%s %s /commands/${folder}/${file}`, '\x1b[42m DEPLOY \x1b[0m', '\x1b[34m[FILE_PATH]\x1b[0m');
 			commands.push(command.data.toJSON());
 		}
 	}
